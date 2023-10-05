1687d068592cf96f5fa0a84f31c8ecca2a331591
Chonlasak Tumna <137064083+ChonlasakThamana@users.noreply.github.com>
2023-10-05 14:16:48 +0700

    11-5

--- Homework/HW11/hw11-5-pass-by-value-and-reference.cpp
+++ Homework/HW11/hw11-5-pass-by-value-and-reference.cpp
@@ -12,5 +12,39 @@
         Max Height = 191
         Max Age = 19
 
-*/
+*/#include <stdio.h>
+
+void findMaxHeightByValue(int Height[], int size) {
+    int max = Height[0];
+    for (int i = 1; i < size; i++) {
+        if (Height[i] > max) {
+            max = Height[i];
+        }
+    }
+    printf("Max Height = %d\n", max);
+}
+
+void findMaxAgeByReference(int Age[], int size, int *maxAge) {
+    *maxAge = Age[0];
+    for (int i = 1; i < size; i++) {
+        if (Age[i] > *maxAge) {
+            *maxAge = Age[i];
+        }
+    }
+}
+
+int main() {
+    int Height[10] = { 159, 168, 191, 188, 154, 152, 167, 165, 178, 177 };
+    int Age[10] = { 18, 19, 18, 17, 16, 18, 17, 18, 18, 18 };
+    
+    int maxAge; // สร้างตัวแปรเพื่อเก็บค่าอายุสูงสุด
+    
+    findMaxHeightByValue(Height, 10);
+    findMaxAgeByReference(Age, 10, &maxAge);
+    
+    printf("Max Age = %d\n", maxAge);
+    
+    return 0;
+}
+
 
